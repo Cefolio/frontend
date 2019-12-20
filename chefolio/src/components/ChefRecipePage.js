5d888d454b5b92ede2b9a16a52ba6d9006f676b8
Chris Vasquez <chrvasq@outlook.com>
2019-12-19 19:55:45 -0800

    Add ChefRecipePage component

---
+++ chefolio/src/components/ChefRecipePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import RecipeCardContainer from "../containers/RecipeCardContainer";
+
+export default class ChefRecipePage extends Component {
+  render() {
+    return (
+      <div>
+        <SearchForm />
+        <RecipeCardContainer />
+      </div>
+    );
+  }
+}
