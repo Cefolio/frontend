dc46f637ba43d3fc5c26f5aa692038b4bce69c64
Chris Vasquez <chrvasq@outlook.com>
2019-12-19 20:04:16 -0800

    Add ChefCard and ChefCardContainer

---
+++ chefolio/src/components/ChefCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ChefCard = props => {
+  /* Need to add data*/
+  return (
+    <div>
+      <h1>Chef Name</h1>
+      <p>Location</p>
+      <p>Email</p>
+      <p>Phone</p>
+    </div>
+  );
+};
+
+export default ChefCard;
