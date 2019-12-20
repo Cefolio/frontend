964866b852f5f53c4245cc0d876daf4330a2e65b
Chris Vasquez <chrvasq@outlook.com>
2019-12-19 19:36:05 -0800

    Add routes to app

--- chefolio/src/App.js
+++ chefolio/src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Homepage from "./components/Homepage";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import ChefRecipePage from "./components/ChefRecipePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="register" component={RegisterForm} />
+        <Route path="/chef/:id/recipes" component={ChefRecipePage} />
+        <PrivateRoute path="/dashboard" component={ChefsDashboard} />
+      </Switch>
     </div>
   );
 }
