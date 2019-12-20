056cf05680528bb869b3c0e00d1249a746d21434
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-12-19 21:02:18 -0800

    working on registerform

--- chefolio/src/components/RegisterForm.js
+++ chefolio/src/components/RegisterForm.js
@@ -13,7 +13,60 @@ const initialUserForm = {
 
 export default function Container(){
     const [serverError, setServerError] = useState('');
-    const addChef = (form)
+    const addChef = (formValues, actions) => {
+        const chefToPost = {
+            username: formValues.username,
+            password: formValues.password
+        };
+        axios
+            .post(RegristrationApi, chefToPost)
+            .then(res => {
+                actions.resetForm();
+            })
+            .catch(err => {
+                debugger;
+            });
+    };
+    return (
+        <div>
+            {serverError}
+            <UserForm onSubmit={addUser}/>
+        </div>
+    );
+}
+
+const validationSchema = yup.object().shape({
+    username: yup.string().required('Please enter your username'),
+    password: yup.string().required('No password provided.').min(4, 'Password is too short -- should be at least 4 characters.').matches(/(?=.*[0-9])/, "Password must contain a number and a special character.")
+});
+const UserForm = ({ onSubmit }) => {
+    return (
+        <Formik
+            validate={validate}
+            validationSchema={validationSchema}
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            render={props => {
+                return(
+                    <div>
+                        <Form>
+                            <h1>Sign Up</h1>
+                            <Field
+                                className='input-field'
+                                name='username'
+                                type='text'
+                                placeholder='Username'
+                                />
+                            <ErrorMessage
+                                className="error-field"
+                                name="username"
+                                component="div"
+                                />
+                        </Form>
+                    </div>
+                )
+            }}
+    )
 }
 
 //===https://github.com/bw-dev-libs/Front-End/blob/master/dev-libs/src/components/Signup.js
