69bb44ac2526d8845d74c8fb9a43ec219697512c
Chris Vasquez <chrvasq@outlook.com>
2019-12-20 20:33:09 -0800

    Added actions for fetchRecipe and editRecipe

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,11 +1,17 @@
 const initialState = {
-    user: {
-        isLoggedIn: false,
-        name: '',
-        email: ''
-    },
-    displayedRecipes: [],
-    isFetching: false,
-    error: ''
+  chef: {
+    isLoggedIn: false,
+    name: "",
+    email: "",
+    location: "",
+    phone: ""
+  },
+  displayedRecipes: [],
+  isFetching: false,
+  error: "",
+  isEditing: false,
+  isSubmitting: false,
+  isDeleting: false
 };
+
 export default initialState;
