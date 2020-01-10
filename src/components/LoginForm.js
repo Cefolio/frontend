2dc06278593fb71f5fa3766aaed0a85c1db1ae49
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-09 22:33:21 -0500

    Fixed login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,54 +1,100 @@
-import React, { useEffect, useRef } from "react";
-import {useForm} from 'react-hook-form'
+import React, { useState } from "react";
+// import {useForm} from 'react-hook-form'
 
 import "../css/LoginForm.scss";
 
 import { connect } from "react-redux";
 import { login } from "../actions/actions";
 
+const LoginForm = props => {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  })
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.login(user, props);
+  }
 
-function LoginForm({ login , ... rest }) {
-  const { register, handleSubmit, errors, formState} = useForm({
-    mode: "onChange"
-  });
-  const onSubmit = async data => {
-    const res = await login(data);
-    if (res) {
-      rest.history.push("/dashboard")
-    } else {
+  const handleChanges = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
 
-    }
-  };
-
-  
-
-  
-  return (
-    <div>
-      <h1>Login</h1>
+    return (
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {errors.text && "Username is needed"}
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            ref={register({ required: true})}/>
-            {errors.password && "Your password is needed"}
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              ref={register({required: true})}/>
-              <div>
-                <button type="submit" disabled={!formState.isValid}>Login</button>
-              </div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={user.email}
+            onChange={handleChanges}
+          />
+          <input 
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChanges}
+          />
+          <button type="submit">Log In</button>
         </form>
       </div>
-    </div>
-  )
+    )
+  
 }
+
+// const LoginForm = () => {
+
+//   const [user, setUser] = useState({
+//     email: "",
+//     password: ""
+//   })
+  // const { register, handleSubmit, errors, formState} = useForm({
+  //   mode: "onChange"
+  // });
+  // const onSubmit = async data => {
+  //   const res = await login(data);
+  //   if (res) {
+  //     rest.history.push("/dashboard")
+  //   } else {
+
+  //   }
+  // };
+
+  
+
+  
+  // return (
+  //   <div>
+  //     <h1>Login</h1>
+  //     <div>
+  //       <form onSubmit={handleSubmit(onSubmit)}>
+  //         {errors.text && "Username is needed"}
+  //         <input
+  //           type="text"
+  //           placeholder="Username"
+  //           name="username"
+  //           ref={register({ required: true})}/>
+  //           {errors.password && "Your password is needed"}
+  //           <input
+  //             type="password"
+  //             placeholder="Password"
+  //             name="password"
+  //             ref={register({required: true})}/>
+  //             <div>
+  //               <button type="submit" disabled={!formState.isValid}>Login</button>
+  //             </div>
+  //       </form>
+  //     </div>
+  //   </div>
+  // )
+// }
 
 
 
