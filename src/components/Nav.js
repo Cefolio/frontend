c3740d5d9146011cece3d5b82262d95ada18fe81
Chris Vasquez <chrvasq@outlook.com>
2020-01-10 22:32:08 -0800

    Update Nav component to use logout action and isLoggedIn state

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,50 +1,48 @@
 // create a fetchChef action in actions
 // create a chef and isFetching state for initialState in reducers
 
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchChef } from '../actions/actions';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../actions/actions";
 // Change to index.scss
 import "../css/Nav.scss";
 
 const Nav = props => {
-
-  const [chef, setChef] = useState(props.chef)
-
-  useEffect(() => {
-    props.fetchChef();
-    setChef(props.chef);
-  }, [props.chef]);
-
   return (
     <div className="nav">
-      {localStorage.getItem('token') ? 
-        <Link to={`/dashboard/${chef.id}`} className="link">
+      {props.loggedIn ? (
+        <Link to={`/dashboard`} className="link">
           Chef Dashboard
         </Link>
-        :
-        <Link to="/" className="link">Home</Link>
-      }
+      ) : (
+        <Link to="/" className="link">
+          Home
+        </Link>
+      )}
+      {props.loggedIn ? null : (
+        <Link to="/register" className="link">
+          Register
+        </Link>
+      )}
 
-      <Link to="/register" className="link">Register</Link>
-
-      {localStorage.getItem('token') ? 
-        <Link to="/" onClick={() => localStorage.clear()} className="link">
+      {props.loggedIn ? (
+        <Link to="/" onClick={() => props.logout()} className="link">
           Logout
         </Link>
-        :
-        <Link to="/login" className="link">Login</Link>
-      }
+      ) : (
+        <Link to="/login" className="link">
+          Login
+        </Link>
+      )}
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => {
   return {
-    chef: state.chef,
-    isFetching: state.isFetching
-  }
-}
+    loggedIn: state.isLoggedIn
+  };
+};
 
-export default connect(mapStateToProps, { fetchChef })(Nav);
+export default connect(mapStateToProps, { logout })(Nav);
